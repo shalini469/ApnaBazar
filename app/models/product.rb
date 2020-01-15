59486a51119692ba2e9d75cff75169118875e661
shalini469 <shalini.dixit@anchanto.com>
2020-01-15 18:55:38 +0530

    new branch added

---
+++ app/models/product.rb
@@ -0,0 +1,5 @@
+class Product < ApplicationRecord
+    attr_accessor :orders_attributes, :order_products_attributes
+    has_many :order_products
+    has_many :orders, through: :order_products
+end
